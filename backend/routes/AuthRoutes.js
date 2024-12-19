4434267beb94365efe0e9958b5c548226e7e7031
Bobur Niyozov <bobur.niyazov0200@gmail.com>
2024-12-19 17:04:59 +0500

    Create AuthRoutes.js

---
+++ backend/routes/AuthRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const AuthController = require('../controllers/AuthController');
+const authenticate = require('../middlewares/AuthMiddleware')
+
+
+const router = express.Router()
+
+router.post('/login',AuthController.loginUser)
+router.post('/register',authenticate,AuthController.registerUser)
+router.post('/refresh',AuthController.refreshToken)
+router.post('/logout',AuthController.logOutUser)
+
+module.exports = router
