337d6ac6687e1d381aa56a0aedae6ad4759d36ed
Bobur Niyozov <bobur.niyazov0200@gmail.com>
2024-12-20 11:09:43 +0500

    Create LoginPage.vue

---
+++ frontend/src/views/LoginPage.vue
@@ -0,0 +1,16 @@
+<script setup>
+import LoginForm from '../components/LoginForm.vue';
+</script>
+
+<template>
+  <div class="container mt-5">
+    <h2 class="text-center">Login</h2>
+    <LoginForm />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  max-width: 500px;
+}
+</style>
