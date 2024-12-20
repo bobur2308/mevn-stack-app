034b191fdf2800bb3906090677852141a8f4f690
Bobur Niyozov <bobur.niyazov0200@gmail.com>
2024-12-20 11:09:37 +0500

    Create RegisterPage.vue

---
+++ frontend/src/views/RegisterPage.vue
@@ -0,0 +1,16 @@
+<script setup>
+import RegisterForm from '../components/RegisterForm.vue';
+</script>
+
+<template>
+  <div class="container mt-5">
+    <h2 class="text-center">Register</h2>
+    <RegisterForm />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  max-width: 500px;
+}
+</style>
