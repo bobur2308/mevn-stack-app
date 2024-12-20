cf51dcc6197d91ccd96ea315134a47a7473b6e2b
Bobur Niyozov <bobur.niyazov0200@gmail.com>
2024-12-20 11:09:38 +0500

    Create ProfilePage.vue

---
+++ frontend/src/views/ProfilePage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Profile Page</h1>
+    <p>Manage your profile settings and details.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProfilePage',
+};
+</script>
+
+<style scoped>
+/* Add styles specific to ProfilePage */
+</style>
