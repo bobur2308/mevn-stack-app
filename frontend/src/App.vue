8ae81973eca207fdff7aa990793d6f1ed083d988
Bobur Niyozov <bobur.niyazov0200@gmail.com>
2024-12-20 11:10:09 +0500

    Create App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <HeaderComponent />
+    <main>
+      <router-view />
+    </main>
+    <FooterComponent />
+  </div>
+</template>
+
+<script>
+import './style.css'; // Ensure your global CSS is included
+import HeaderComponent from './components/HeaderComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent,
+    FooterComponent,
+  },
+};
+</script>
+
+
+<style>
+main {
+  min-height: 80vh; /* Adjust height to fit Header and Footer */
+  padding: 20px;
+}
+</style>
