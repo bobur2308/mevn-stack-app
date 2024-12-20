115874491c44d44075fba8e8d217b6a0d4d72f6b
Bobur Niyozov <bobur.niyazov0200@gmail.com>
2024-12-20 08:41:45 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser');
 const connectDb = require('./config/db')
+const path = require('path')
 const app = express()
 
 
@@ -10,6 +11,7 @@ app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({extended:true}))
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
 app.get('/',(req,res)=>{
   res.send("Hello world !")
@@ -17,6 +19,7 @@ app.get('/',(req,res)=>{
 
 // routes
 app.use('/api/auth',require('./routes/AuthRoutes'))
+app.use('/api/post',require('./routes/PostRoutes'))
 
 
 connectDb()
