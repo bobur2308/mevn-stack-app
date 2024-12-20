a5b0c5bacbe5afac9152614824d6799f54af2838
Bobur Niyozov <bobur.niyazov0200@gmail.com>
2024-12-20 11:09:50 +0500

    Create index.js

---
+++ frontend/src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+// Import views
+import HomePage from '../views/HomePage.vue';
+import PostPage from '../views/PostsPage.vue';
+import ProfilePage from '../views/ProfilePage.vue';
+import LoginPage from '../views/LoginPage.vue';
+import RegisterPage from '../views/RegisterPage.vue';
+
+const routes = [
+  { path: '/', name: 'Home', component: HomePage },
+  { path: '/post', name: 'Post', component: PostPage },
+  { path: '/profile', name: 'Profile', component: ProfilePage },
+  { path: '/login', name: 'Login', component: LoginPage },
+  { path: '/register', name: 'Register', component: RegisterPage },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
